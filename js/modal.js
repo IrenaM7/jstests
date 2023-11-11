89e38cfc2ea94a0beb937c48fd563f502772d5c7
IrenaM7 <irachereshnya16@gmail.com>
2023-11-11 08:05:22 +0100

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,64 @@
-let ageUser = 40;
-console.log(ageUser);
-ageUser += 15;
-console.log(ageUser);
-console.log(typeof ageUser);
-console.log(typeof true);
-const a = 10;
-const b = 5;
-console.log(a + b);
-console.log(a % b);
-console.log(a ** b);
-const result = a + b;
-console.log(result);
-const names = "Irena";
-const sourName = "Malinowska";
-const nam = `${names} ${sourName}`;
-console.log(nam);
+/**Станція з продажу ремонтних дроїдів готова до запуску, 
+  * залишилося написати програмне забезпечення для відділу 
+  * продажів.
+
+Оголоси функцію makeTransaction, яка очікує два параметри, 
+значення яких будуть задаватися під час її виклику:
+
+quantity— перший параметр, число, що містить кількість 
+замовлених дроїдів
+pricePerDroid — другий параметр, число, що містить вартість
+ одного дроїда
+Доповни код функції так, щоб вона повертала рядок 
+з повідомленням про покупку ремонтних 
+дроїдів: "You ordered <quantity> droids worth <totalPrice> 
+credits!", де:
+
+<quantity> — це кількість замовлених дроїдів
+<totalPrice> — це загальна вартість замовлення, 
+тобто вартість усіх замовлених дроїдів
+Візьми код нижче і встав після оголошення своєї функції 
+для перевірки коректності її роботи. У консоль будуть 
+виведені результати її роботи.
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+Залиш це */
+/**задача 1 */
+
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  const result = `You ordered ${quantity} droids worth ${totalPrice} 
+  credits!`;
+  return result;
+}
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+/**Задача 2. */
+
+function getShippingMessage(country, price, deliveryFee) {
+  const totalPrice = price + deliveryFee;
+  const result = `Shipping to ${country} will cost ${totalPrice} credits`;
+  return result;
+}
+
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20));
+
+/** задача 3 */
+
+function getElementWidth(content, padding, border) {
+  const elementWidth =
+    Number.parseInt(content) +
+    Number.parseInt(padding) * 2 +
+    Number.parseFloat(border) * 2;
+  return elementWidth;
+}
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
